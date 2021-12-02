d8dfed372c034aa6e81169a0435b9fa017192a1c
Sjoerd Bolten <sjoerdabolten@gmail.com>
2021-12-02 11:55:15 +0100

    day 2 - oneliner

--- day02.js
+++ day02.js
@@ -3,63 +3,35 @@
 // ======
 // Part 1
 // ======
-const part1 = (input) => {
-  const parsed = parseInput(input);
-
-  let x = 0,
-    y = 0;
-
-  for (let i = 0; i < parsed.length; i++) {
-    const { move, count } = parsed[i];
-    switch (move) {
-      case "up":
-        x -= count;
-        break;
-      case "down":
-        x += count;
-        break;
-      case "forward":
-        y += count;
-        break;
-      default:
-        console.log("fuck");
-    }
-  }
-
-  return x * y;
-};
+const part1 = (input) =>
+  input
+    .split("\n")
+    .map((l) => [l.split(" ")[0], parseInt(l.split(" ")[1])])
+    .reduce(
+      ([x, y], [m, c]) => [
+        (m == "up" && (x -= c)) || (m == "down" && (x += c)) || x,
+        (m == "forward" && (y += c)) || y,
+      ],
+      [0, 0]
+    )
+    .reduce((p, c) => p * c, 1);
 
 // ======
 // Part 2
 // ======
-const part2 = (input) => {
-  const parsed = parseInput(input);
-
-  let x = 0,
-    y = 0,
-    aim = 0;
-
-  for (let i = 0; i < parsed.length; i++) {
-    const { move, count } = parsed[i];
-    if (move == "up") {
-      aim -= count;
-    } else if (move == "down") {
-      aim += count;
-    } else if (move == "forward") {
-      x += count;
-      y += aim * count;
-    }
-  }
-
-  return x * y;
-};
-
-const parseInput = (input) =>
-  input.split("\n").map((x) => {
-    return {
-      move: x.split(" ")[0],
-      count: parseInt(x.split(" ")[1]),
-    };
-  });
+const part2 = (input) =>
+  input
+    .split("\n")
+    .map((l) => [l.split(" ")[0], parseInt(l.split(" ")[1])])
+    .reduce(
+      ([x, y, a], [m, c]) => [
+        (m == "forward" && (x += c)) || x,
+        (m == "forward" && (y += a * c)) || y,
+        (m == "up" && (a -= c)) || (m == "down" && (a += c)) || a,
+      ],
+      [0, 0, 0]
+    )
+    .slice(0, 2)
+    .reduce((p, c) => p * c, 1);
 
 module.exports = { part1, part2 };
