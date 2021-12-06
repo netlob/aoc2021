8a30e5b34c408d11a72d2dcc49aac5db4cd18277
Sjoerd Bolten <sjoerdabolten@gmail.com>
2021-12-06 07:24:43 +0100

    day 2 part 1 optimised

--- day06.ts
+++ day06.ts
@@ -5,23 +5,24 @@
 // ======
 const part1 = (input) => {
   console.time("part1");
-  let fishes = parseInput(input);
+  const fishes = parseInput(input);
 
-  for (let i = 0; i < 80; i++) {
-    const newFishes = [];
-    fishes = fishes
-      .map((fish) => {
-        if (fish > 0) return fish - 1;
-        if (fish == 0) {
-          newFishes.push(8);
-          return 6;
-        }
-      })
-      .concat(newFishes);
+  let days = 80;
+  let makingFishes = [0];
+  fishes.forEach((f) => (makingFishes[f] = makingFishes[f] + 1 || 1));
+  let total = 0;
+  for (let i = 0; i < days; i++) {
+    if (i in makingFishes) {
+      const newFish = makingFishes[i];
+      makingFishes[i + 7] = makingFishes[i + 7] + newFish || newFish;
+      makingFishes[i + 9] = makingFishes[i + 9] + newFish || newFish;
+      if (i + 7 >= days) total += newFish;
+      if (i + 9 >= days) total += newFish;
+    }
   }
 
   console.timeEnd("part1");
-  return fishes.length;
+  return total;
 };
 
 // ======
